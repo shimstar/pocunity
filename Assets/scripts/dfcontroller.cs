a9d8e4fc39af8b7449e86e3fc0caa5e6a8d67f37
thyshimrod <shimrod@free.fr>
2016-04-08 22:54:51 +0200

    import initial

---
+++ Assets/scripts/dfcontroller.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dfcontroller : MonoBehaviour {
+    private float torqueStep = 1;
+    private float speed=0;
+    private float speedMax = 100;
+
+    void FixedUpdate()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (Input.GetKey(KeyCode.Z)==true)
+        {
+            rb.AddTorque(transform.right * torqueStep);
+        }
+        else if (Input.GetKey(KeyCode.S) == true)
+        {
+            rb.AddTorque(transform.right * -torqueStep);
+        }
+        else if (Input.GetKey(KeyCode.D) == true)
+        {
+            rb.AddTorque(transform.up* -torqueStep);
+        }
+        else if (Input.GetKey(KeyCode.Q) == true)
+        {
+            rb.AddTorque(transform.up* torqueStep);
+        }
+        else if (Input.GetKey(KeyCode.A) == true)
+        {
+            
+            if ((speed+(Time.deltaTime * 10)) > speedMax)
+            {
+                speed = speedMax;
+            }
+            else
+            {
+                speed += Time.deltaTime * 10;
+            }
+            
+      
+        }
+        else if (Input.GetKey(KeyCode.W) == true)
+        {
+
+            if ((speed - (Time.deltaTime * 10)) < 0)
+            {
+                speed = 0;
+            }
+            else
+            {
+                speed -= Time.deltaTime * 10;
+            }
+
+
+        }
+
+        rb.AddForce(rb.transform.forward * -speed, ForceMode.Acceleration);
+
+        //  rb.AddForce(transform.forward * 100);
+    }
+
+    
+}
---
+++ Assets/scripts/networkmanager.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class networkmanager : MonoBehaviour {
+
+    public bool isAtStartup = true;
+    NetworkClient client;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isAtStartup)
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                SetupServer();
+            }
+
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                SetupClient();
+            }
+
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                SetupServer();
+                SetupLocalClient();
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        if (isAtStartup)
+        {
+            GUI.Label(new Rect(2, 10, 150, 100), "Press S for server");
+            GUI.Label(new Rect(2, 30, 150, 100), "Press B for both");
+            GUI.Label(new Rect(2, 50, 150, 100), "Press C for client");
+        }
+    }
+
+    public void SetupServer()
+    {
+        NetworkServer.Listen(4444);
+        isAtStartup = false;
+        Application.LoadLevel("level1");
+        Debug.Log("pppp");
+
+    }
+
+    // Create a client and connect to the server port
+    public void SetupClient()
+    {
+        client = new NetworkClient();
+        client.RegisterHandler(MsgType.Connect, OnConnected);
+        client.Connect("127.0.0.1", 4444);
+        isAtStartup = false;
+    }
+
+    // Create a local client and connect to the local server
+    public void SetupLocalClient()
+    {
+        client = ClientScene.ConnectLocalServer();
+        client.RegisterHandler(MsgType.Connect, OnConnected);
+        isAtStartup = false;
+    }
+
+    public void OnConnected(NetworkMessage netMsg)
+    {
+        Debug.Log("Connected to server");
+    }
+}
