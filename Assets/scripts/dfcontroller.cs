7aaf58d8f0f90185db703ff99a0de08f4c13f2b3
thyshimrod <shimrod@free.fr>
2016-04-09 21:59:38 +0200

    Modification des torques

--- Assets/scripts/dfcontroller.cs
+++ Assets/scripts/dfcontroller.cs
@@ -2,61 +2,74 @@
 using System.Collections;
 
 public class dfcontroller : MonoBehaviour {
-    private float torqueStep = 1;
+    private float torqueStep = 3;
     private float speed=0;
     private float speedMax = 100;
+    private Rigidbody rb;
 
-    void FixedUpdate()
+    void start()
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
-        if (Input.GetKey(KeyCode.Z)==true)
+         rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (rb)
         {
-            rb.AddTorque(transform.right * torqueStep);
-        }
-        else if (Input.GetKey(KeyCode.S) == true)
-        {
-            rb.AddTorque(transform.right * -torqueStep);
-        }
-        else if (Input.GetKey(KeyCode.D) == true)
-        {
-            rb.AddTorque(transform.up* -torqueStep);
-        }
-        else if (Input.GetKey(KeyCode.Q) == true)
-        {
-            rb.AddTorque(transform.up* torqueStep);
-        }
-        else if (Input.GetKey(KeyCode.A) == true)
-        {
-            
-            if ((speed+(Time.deltaTime * 10)) > speedMax)
+            if (Input.GetKey(KeyCode.Z) == true)
             {
-                speed = speedMax;
+                rb.AddTorque(transform.right * torqueStep * Time.deltaTime);
             }
-            else
+            else if (Input.GetKey(KeyCode.S) == true)
             {
-                speed += Time.deltaTime * 10;
+                rb.AddTorque(transform.right * -torqueStep * Time.deltaTime);
             }
-            
-      
+            else if (Input.GetKey(KeyCode.D) == true)
+            {
+                rb.AddTorque(transform.up * -torqueStep * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.Q) == true)
+            {
+                rb.AddTorque(transform.up * torqueStep * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.A) == true)
+            {
+
+                if ((speed + (Time.deltaTime * 10)) > speedMax)
+                {
+                    speed = speedMax;
+                }
+                else
+                {
+                    speed += Time.deltaTime * 10;
+                }
+
+
+            }
+            else if (Input.GetKey(KeyCode.W) == true)
+            {
+
+                if ((speed - (Time.deltaTime * 10)) < 0)
+                {
+                    speed = 0;
+                }
+                else
+                {
+                    speed -= Time.deltaTime * 10;
+                }
+
+
+            }
+
+            rb.AddForce(rb.transform.forward * -speed, ForceMode.Acceleration);
+
         }
-        else if (Input.GetKey(KeyCode.W) == true)
+        else
         {
-
-            if ((speed - (Time.deltaTime * 10)) < 0)
-            {
-                speed = 0;
-            }
-            else
-            {
-                speed -= Time.deltaTime * 10;
-            }
-
-
+            rb = GetComponent<Rigidbody>();
         }
-
-        rb.AddForce(rb.transform.forward * -speed, ForceMode.Acceleration);
-
-        //  rb.AddForce(transform.forward * 100);
+        
+        
     }
 
     
