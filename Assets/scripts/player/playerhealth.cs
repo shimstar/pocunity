557522e6f0db617c36940b3d87b4b2f9d71fdc2b
thyshimrod <shimrod@free.fr>
2016-04-10 14:58:24 +0200

    debut gestion barre de vie

---
+++ Assets/scripts/player/playerhealth.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class playerhealth : MonoBehaviour {
+    public float hull;
+    public float maxHull = 100;
+    public Slider HealthBar;
+
+    // Use this for initialization
+    void Start () {
+        hull = maxHull;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
