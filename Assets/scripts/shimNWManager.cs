a9899b26b3b31880a75f5ca6efd3f5bad426dd90
thyshimrod <shimrod@free.fr>
2016-04-15 14:27:18 +0200

    spawn effectif ... mouvement un peu laborieux

--- Assets/scripts/shimNWManager.cs
+++ Assets/scripts/shimNWManager.cs
@@ -4,17 +4,36 @@ using UnityEngine.Networking;
 
 public class shimNWManager : NetworkManager {
 
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        playerPrefab = Resources.Load("dark_fighter_631") as GameObject;
+    }
+
     public override void OnServerConnect(NetworkConnection conn)
     {
         base.OnServerConnect(conn);
-        GameObject df = (GameObject)Instantiate(Resources.Load("dark_fighter_631"));
-        NetworkServer.Spawn(df);
+
+        // NetworkServer.Spawn(df);
     }
 
     public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
     {
+        GameObject pref = Resources.Load("dark_fighter_631") as GameObject;
+        ClientScene.RegisterPrefab(pref);
         base.OnServerAddPlayer(conn, playerControllerId);
-        Debug.Log("POGI2");
+        Debug.Log("porpoarp");
+        //Debug.Log("ARFARF " + playerControllerId);
+        GameObject df = (GameObject)Instantiate(pref);
+        //NetworkServer.AddPlayerForConnection(conn, df, playerControllerId);
     }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log("qsdqsdq");
+        base.OnClientConnect(conn);
+        ClientScene.AddPlayer(0);
+    }
+
 
 }
