1f06a60944b6899807246d03bdb84ca42a77a3b5
thyshimrod <shimrod@free.fr>
2016-04-22 22:27:37 +0200

    destruction d'un bullet au bout de 20 sec

--- Assets/scripts/bulletscript.cs
+++ Assets/scripts/bulletscript.cs
@@ -4,16 +4,23 @@ using UnityEngine.Networking;
 
 public class bulletscript : NetworkBehaviour
 {
-
-	// Use this for initialization
-	void Start () {
-	
+    private float timer;
+    // Use this for initialization
+    void Start () {
+        timer = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (timer > 20)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
+    }
 
     void OnCollisionEnter(Collision collision)
     {
