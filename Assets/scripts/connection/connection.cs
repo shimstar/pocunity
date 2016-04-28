dc875c6725e505ea69e440df018ba34d0d8d1fa9
thyshimrod <shimrod@free.fr>
2016-04-28 22:48:38 +0200

    connexion repo rest ok

--- Assets/scripts/connection/connection.cs
+++ Assets/scripts/connection/connection.cs
@@ -2,6 +2,17 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.Networking;
+using System.Text;
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public class login
+{
+    public string code;
+    public string status;
+    public string id;
+}
 
 public class connection : MonoBehaviour {
     private NetworkClient client;
@@ -18,12 +29,36 @@ public class connection : MonoBehaviour {
 
     public void onClick()
     {
-        Debug.Log("connect");
-        SceneManager.LoadScene("scenes/level1");
-      /*  client = new NetworkClient();
-        client.RegisterHandler(MsgType.Connect, OnConnected);
-        client.Connect("127.0.0.1", 7777);*/
+        //Debug.Log("connect");
+        //  SceneManager.LoadScene("scenes/level1");
+
+        string jsonData = "{\"login\":\"test\",\"password\":\"test\"}";
+        Dictionary<string, string> data = new Dictionary<string, string>();
+       
+        data.Add("Content-Type", "application/json");
+        byte[] pData = Encoding.ASCII.GetBytes(jsonData.ToCharArray());
+        
+        WWW www = new WWW("http://127.0.0.1:15881/connect",pData,data);
+        StartCoroutine("GetdataEnumerator", www);
+
     }
+
+       IEnumerator GetdataEnumerator(WWW www)
+    {
+        //Wait for request to complete
+        yield return null;
+        yield return www;
+    
+        if (!string.IsNullOrEmpty(www.text))
+        {
+            var login = JsonUtility.FromJson<login>(www.text);
+       
+        }
+       
+    }
+
+
+
 
     public void OnConnected(NetworkMessage netMsg)
     {
