2c4c5edddb303fcd60554af16b93f1384d5c2175
thyshimrod <shimrod@free.fr>
2016-05-01 22:30:56 +0200

    affichage reticle centre écran

--- Assets/ghoulscript.cs
+++ Assets/ghoulscript.cs
@@ -2,14 +2,21 @@
 using System.Collections;
 
 public class ghoulscript : ship {
+    private GameObject targetUI;
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        targetUI = (GameObject)Instantiate(targetUIPrefab);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
+        GameObject playerShip = GameObject.Find("PlayerShip");
+        if (playerShip != null)
+        {
+            Camera cam = Camera.main;
+            targetUI.transform.position = cam.ViewportToWorldPoint(transform.position);
+        }
+
 	}
 }
--- Assets/scripts/player/dfcontroller.cs
+++ Assets/scripts/player/dfcontroller.cs
@@ -8,6 +8,7 @@ using System;
 public class dfcontroller : ship
 {
     public GameObject greenLaserPrefab;
+    public Texture2D crosshairImage;
 
     public float getPrcentSpeed() {
         return speed / speedMax;
@@ -18,6 +19,12 @@ public class dfcontroller : ship
         return hull / maxHull;
     }
 
+    void OnGUI()
+    {
+        float xMin = (Screen.width / 2) - (crosshairImage.width / 2);
+        float yMin = (Screen.height / 2) - (crosshairImage.height / 2);
+        GUI.DrawTexture(new Rect(xMin, yMin, crosshairImage.width, crosshairImage.height), crosshairImage);
+    }
 
     public override void OnStartLocalPlayer()
     {
