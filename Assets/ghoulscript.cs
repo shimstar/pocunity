3a5cbb8134a2366c47f3ab4aaf14678b5644ccf1
thyshimrod <shimrod@free.fr>
2016-05-15 23:00:16 +0200

    premiers test AI : orientation vers cube

--- Assets/ghoulscript.cs
+++ Assets/ghoulscript.cs
@@ -3,17 +3,40 @@ using System.Collections;
 
 public class ghoulscript : ship {
     private GameObject targetUI;
+    private GameObject goPoint;
+    public float force = 0.1f;
+    
 
-	// Use this for initialization
-	void Start () {
-        
+    // Use this for initialization
+    void Start () {
+        goPoint = GameObject.Find("gopoint");
+        this.rb = GetComponent<Rigidbody>();
+        speed = 200;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        
+        if (isServer) {
 
-	}
+            /* if(goPoint != null)
+             {
+                 Vector3 targetDelta = goPoint.transform.position - transform.position;
+                 //get the angle between transform.forward and target delta
+                 float angleDiff = Vector3.Angle(transform.forward, targetDelta);
+
+                 // get its cross product, which is the axis of rotation to
+                 // get from one vector to the other
+                 Vector3 cross = Vector3.Cross(transform.forward, targetDelta);
+
+                 // apply torque along that axis according to the magnitude of the angle.
+                 rb.AddTorque(cross * angleDiff * force,ForceMode.Impulse);
+                 Debug.Log(cross * angleDiff * force);
+             }*/
+            rb.AddForce(rb.transform.forward * -speed, ForceMode.Acceleration);
+        }
+
+
+    }
 
     void OnGUI()
     {
