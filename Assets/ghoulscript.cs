649c125a44366ed85fcb70510968728b234e4f60
thyshimrod <shimrod@free.fr>
2016-05-16 23:02:03 +0200

    nouveau modele retourné  + début torque vers direction

--- Assets/ghoulscript.cs
+++ Assets/ghoulscript.cs
@@ -4,35 +4,72 @@ using System.Collections;
 public class ghoulscript : ship {
     private GameObject targetUI;
     private GameObject goPoint;
+    private GameObject lookAtSphere;
     public float force = 0.1f;
+    private float torque = 1.0f;
     
 
     // Use this for initialization
     void Start () {
         goPoint = GameObject.Find("gopoint");
         this.rb = GetComponent<Rigidbody>();
-        speed = 200;
+        speed = 50;
+        lookAtSphere = GameObject.Find("/" + this.name + "/lookat");
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (isServer) {
 
-            /* if(goPoint != null)
+             if(goPoint != null)
              {
-                 Vector3 targetDelta = goPoint.transform.position - transform.position;
-                 //get the angle between transform.forward and target delta
-                 float angleDiff = Vector3.Angle(transform.forward, targetDelta);
+                lookAtSphere.transform.LookAt(goPoint.transform);
+                
+                
+                float diffX = transform.rotation.x - lookAtSphere.transform.rotation.x;
+                float diffY = transform.rotation.y - lookAtSphere.transform.rotation.y;
+                float diffZ = transform.rotation.z - lookAtSphere.transform.rotation.z;
+                Debug.Log(diffX +"/" + diffY + "/" +diffZ);
 
-                 // get its cross product, which is the axis of rotation to
-                 // get from one vector to the other
-                 Vector3 cross = Vector3.Cross(transform.forward, targetDelta);
+                if (diffX < 0)
+                {
+                    rb.AddTorque(transform.up * torque, ForceMode.Acceleration);
+                }
+                else
+                {
+                    if (diffX > 0.1)
+                    {
+                        rb.AddTorque(transform.up * -torque, ForceMode.Acceleration);
+                    }
+                }
+                /*
+               if(cross.x < -5)
+               {
+                   rb.AddTorque(transform.up * torque, ForceMode.Acceleration);
+               }else
+               {
+                   if(cross.x > 5)
+                   {
+                       rb.AddTorque(transform.up * -torque, ForceMode.Acceleration);
+                   }
+               }
 
-                 // apply torque along that axis according to the magnitude of the angle.
-                 rb.AddTorque(cross * angleDiff * force,ForceMode.Impulse);
-                 Debug.Log(cross * angleDiff * force);
-             }*/
-            rb.AddForce(rb.transform.forward * -speed, ForceMode.Acceleration);
+               if(cross.y < -5)
+               {
+                   rb.AddTorque(transform.right * torque, ForceMode.Acceleration);
+               }else
+               {
+                   if (cross.y > 5)
+                       rb.AddTorque(transform.right * -torque, ForceMode.Acceleration);
+               }
+
+                // apply torque along that axis according to the magnitude of the angle.
+                //rb.AddTorque(cross * angleDiff * force,ForceMode.Impulse);
+                */
+
+
+            }
+           // rb.AddForce(rb.transform.forward * speed, ForceMode.Acceleration);
         }
 
 
