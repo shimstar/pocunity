5c57bb6094a6750114af374382b0a38d94f44d8c
thyshimrod <shimrod@free.fr>
2016-05-19 13:47:00 +0200

    patrouille entre 3 points en mode torque en utilisant la transformation en position locale.

--- Assets/ghoulscript.cs
+++ Assets/ghoulscript.cs
@@ -3,59 +3,35 @@ using System.Collections;
 
 public class ghoulscript : ship {
     private GameObject targetUI;
-    private GameObject goPoint;
+    
     private GameObject lookAtSphere;
     public float force = 0.1f;
     private float torque = 1.0f;
+    private int gopointNb = 0;
+    private ArrayList patrolPoints = new ArrayList();
     
 
     // Use this for initialization
     void Start () {
-        goPoint = GameObject.Find("gopoint");
         this.rb = GetComponent<Rigidbody>();
         speed = 50;
         lookAtSphere = GameObject.Find("/" + this.name + "/lookat");
+        torqueStep = 50;
+        patrolPoints.Add(new Vector3(0, 2, 405));
+        patrolPoints.Add(new Vector3(2100, 3, 1145));
+        patrolPoints.Add(new Vector3(750, 3, -330));
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (isServer) {
 
-             if(goPoint != null)
-             {
-                /// hummmm
-                Vector3 direction = goPoint.transform.position  - transform.position;
-                float angleDiff = Vector3.Angle(transform.up, direction);
-                Vector3 cross = Vector3.Cross(transform.up, direction);
-                rb.AddTorque(transform.up * direction.magnitude * Time.deltaTime, ForceMode.Acceleration);
 
-                /// using slerp
-               /* Vector3 direction = goPoint.transform.position  - transform.position;
-                direction.z = -direction.z;
-                Quaternion rotation = Quaternion.LookRotation(direction);
-                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime);*/
-                //rb.AddForce(rb.transform.forward * speed * Time., ForceMode.Acceleration); 
-
-                /// using moveRotation
-                /*Vector3 direction = goPoint.transform.position - transform.position;
-                Vector3 localPoint = transform.InverseTransformDirection(goPoint.transform.position);
-                float angle = Mathf.Atan2(localPoint.x, localPoint.z) * Mathf.Rad2Deg;
-                Vector3 eulerAngleVelocity = new Vector3(0, angle, 0);
-
-                Quaternion deltaRotation = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime);
-                rb.MoveRotation(rb.rotation * deltaRotation);
-
-                angle = Mathf.Atan2(localPoint.y, localPoint.x) * Mathf.Rad2Deg;
-                eulerAngleVelocity = new Vector3(0, 0, angle);
-                deltaRotation = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime);
-                rb.MoveRotation(rb.rotation * deltaRotation);
-
-                rb.AddForce(rb.transform.forward * speed, ForceMode.Acceleration);*/
-
+            if (patrolPoints.Count >= 1) { 
                 /// using torque
                 //Vector3 localPoint = rb.transform.InverseTransformPoint(goPoint.transform.position);
-                /* Vector3 localPoint = transform.InverseTransformDirection(goPoint.transform.position - transform.position);
-                 float distance =Mathf.Sqrt(goPoint.transform.position.x - transform.position.x) * (goPoint.transform.position.x - transform.position.x) + (goPoint.transform.position.x - transform.position.x) * (goPoint.transform.position.x - transform.position.x) + (goPoint.transform.position.y - transform.position.y) * (goPoint.transform.position.z - transform.position.z);
+                 Vector3 localPoint = transform.InverseTransformDirection(transform.position - (Vector3)patrolPoints[gopointNb] );
+                 float distance =Mathf.Sqrt(Mathf.Abs((((Vector3)patrolPoints[gopointNb]).x - transform.position.x) * (((Vector3)patrolPoints[gopointNb]).x - transform.position.x) + (((Vector3)patrolPoints[gopointNb]).y - transform.position.y) * (((Vector3)patrolPoints[gopointNb]).y - transform.position.y) + (((Vector3)patrolPoints[gopointNb]).z - transform.position.z) * (((Vector3)patrolPoints[gopointNb]).z - transform.position.z)));
                  Debug.Log(localPoint + "////" + localPoint.magnitude + "////" + distance);
                  //Debug.Log(lookAtSphere.transform.position + "///" + transform.position);
                  float factor = 0.0f;
@@ -65,15 +41,17 @@ public class ghoulscript : ship {
 
                  rb.AddTorque(rb.transform.up * torqueStep * Time.deltaTime * factor, ForceMode.Acceleration);
 
-                 factor = factor == 0 ? (localPoint.y > 20 ? 1.0f : (localPoint.y < -20 ? -1.0f : (localPoint.y > 2 ? 0.25f : (localPoint.y < -2 ? -0.25f : 0)))): 0;
+                 factor = factor < 1 ? (localPoint.y > 20 ? 1.0f : (localPoint.y < -20 ? -1.0f : (localPoint.y > 2 ? 0.25f : (localPoint.y < -2 ? -0.25f : 0)))): 0;
                  rb.AddTorque(rb.transform.right * torqueStep * Time.deltaTime * factor, ForceMode.Acceleration);
 
-                 if (factor==0) rb.AddForce(rb.transform.forward * speed, ForceMode.Acceleration);*/
+                 rb.AddForce(rb.transform.forward * speed, ForceMode.Acceleration);
                 //Debug.Log(a);
-
-
+                if (distance < 100)
+                {
+                    gopointNb += 1;
+                    gopointNb %= 3;
+                }
             }
-           //
         }
 
 
