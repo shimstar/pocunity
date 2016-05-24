088c8aafe4bb075f439c1120beb653a0396774df
thyshimrod <shimrod@free.fr>
2016-05-24 23:11:08 +0200

    correction apparition asteroid

--- Assets/level1StartScript.cs
+++ Assets/level1StartScript.cs
@@ -8,20 +8,23 @@ public class level1StartScript : NetworkBehaviour
     void generateLevel()
     {
         GameObject asteroidPref = Resources.Load("zone/asteroid1") as GameObject;
-        int nbAmas = Random.Range(1, 10);
+        int nbAmas = Random.Range(1, 100);
         for (int itAmas = 0; itAmas < nbAmas; itAmas++)
         {
             GameObject ast = (GameObject)Instantiate(asteroidPref);
-            ast.transform.position.Set(Random.Range(-100000, 100000), Random.Range(-100000, 100000), Random.Range(-100000, 100000));
+            ast.transform.position=new Vector3(Random.Range(-5000, 5000), Random.Range(-5000, 5000), Random.Range(-5000, 5000));
+            
             ast.transform.Rotate( Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
             ast.name = "Asteroid" + itAmas;
-            int nbAst = Random.Range(1, 10);
+            int nbAst = Random.Range(10, 100);
             for (int itNbAst=0; itNbAst < nbAst; itNbAst++)
             {
                 GameObject ast2 = (GameObject)Instantiate(asteroidPref);
-                ast2.transform.position.Set(ast.transform.position.x + Random.Range(-10000, 10000), ast.transform.position.y + Random.Range(-10000, 10000), ast.transform.position.z + Random.Range(-10000, 10000));
+                ast2.transform.position = new Vector3(ast.transform.position.x + Random.Range(-1000, 1000), ast.transform.position.y + Random.Range(-1000, 1000), ast.transform.position.z + Random.Range(-1000, 1000));
                 ast2.transform.Rotate(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
                 ast2.name = "Asteroid" + itAmas + "_" + itNbAst;
+                int scale = Random.Range(1, 4);
+                ast2.transform.localScale = new Vector3(scale, scale, scale);
             }
         }
     }
